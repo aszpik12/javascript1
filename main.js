017378c093a87b0fdb372ef3868b4054a3a6c1fb
Asztrik <asztrikpetrovai@gmail.com>
2023-02-17 10:58:14 +0100

    java

---
+++ main.js
@@ -0,0 +1,25 @@
+
+    window.addEventListener("load", init)
+
+    function kismaqcska(){
+        console.log("itt vagyok");
+
+        valtozok();
+    }
+
+
+    function valtozok(){
+        var vnev1;
+        let vnev2=3.45;
+        const vNEV3 = 4;
+        vnev = 1
+        let vmi = 3<2;
+        console.log("vnev1", vnev1, typeof vnev1)
+        console.log("vnev2", vnev2, typeof vnev2)
+        console.log("vnev3", vNEV3, typeof vNEV3)
+        console.log("vmi", vmi, typeof vmi)
+
+        var eredmeny = vnev1+vnev2
+        console.log("eredmeny", eredmeny, typeof eredmeny)
+    }
+
